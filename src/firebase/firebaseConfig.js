d25a38536a4d8ced328742aba9300b309085743e
Nusrat Jahan <nusratjahanmithila66@gmail.com>
2024-08-31 23:34:33 +0600

    env file updated

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,20 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAIPS00f_56pbPvD63_MMYPKgdKUX442fw",
-  authDomain: "tracklocation-c12fe.firebaseapp.com",
-  databaseURL: "https://tracklocation-c12fe-default-rtdb.firebaseio.com",
-  projectId: "tracklocation-c12fe",
-  storageBucket: "tracklocation-c12fe.appspot.com",
-  messagingSenderId: "492761724996",
-  appId: "1:492761724996:web:b10c3858fbe638f23ea411",
-  measurementId: "G-4RPX74NDHR",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  databaseURL: import.meta.env.VITE_databaseURL,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
