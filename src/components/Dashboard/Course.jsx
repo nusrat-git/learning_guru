c3da614a64231879e123d1d3d761926345bc85cf
Nusrat Jahan <nusratjahanmithila66@gmail.com>
2024-08-31 20:46:46 +0600

    some changes

--- src/components/Dashboard/Course.jsx
+++ src/components/Dashboard/Course.jsx
@@ -5,8 +5,10 @@ import {
   updateStudentCourseStatus,
 } from "../../redux/thunks/courseThunks";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 const Course = ({ course }) => {
+  console.log(course);
   const { user } = useSelector((state) => state.auth);
   const { loading } = useSelector((state) => state.courses);
   const dispatch = useDispatch();
@@ -14,6 +16,7 @@ const Course = ({ course }) => {
   const courseId = course?.key;
   const studentId = course?.studentKey;
   const email = user?.email;
+  const randomWidth = `${Math.floor(Math.random() * 100) + 1}%`;
 
   function getFormattedEndDate(enrollmentDateStr, courseDurationStr) {
     const enrollmentDate = new Date(enrollmentDateStr);
@@ -49,29 +52,51 @@ const Course = ({ course }) => {
           <h1 className="font-bold mb-3 tracking-tight leading-none text-2xl text-white">
             {course.name}
           </h1>
-          <button onClick={handleComplete}>Complete</button>
+          {course?.completeStatus === "In progress" && (
+            <button onClick={handleComplete}>Complete</button>
+          )}
         </div>
         <p className="mb-2 font-light text-gray-200 md:text-lg">
           <span className="font-semibold">Instructor:</span> {course.instructor}
         </p>
-        <p className="mb-2 font-light text-gray-200 md:text-lg">
-          <span className="font-semibold">Due date:</span>{" "}
-          {getFormattedEndDate(course?.enrollDate, course?.duration)}
-        </p>
-        <div>
-          <div className="flex justify-between mb-1">
-            <span className="text-base font-medium text-blue-300">
-              Completed
-            </span>
-            <span className="text-sm font-medium text-blue-300">45%</span>
+        {course?.enrollDate ? (
+          <div>
+            <p className="mb-2 font-light text-gray-200 md:text-lg">
+              <span className="font-semibold">Due date:</span>{" "}
+              {getFormattedEndDate(course?.enrollDate, course?.duration)}
+            </p>
+            <div>
+              <div className="flex justify-between mb-1">
+                <span className="text-base font-medium text-blue-300">
+                  Completed
+                </span>
+                <span className="text-sm font-medium text-blue-300">
+                  {course?.completeStatus === "Completed"
+                    ? "100%"
+                    : randomWidth}
+                </span>
+              </div>
+              <div className="w-full bg-gray-200 rounded-full h-2.5">
+                <div
+                  className="bg-blue-300 h-2.5 rounded-full"
+                  style={{
+                    width:
+                      course?.completeStatus === "Completed"
+                        ? "100%"
+                        : randomWidth,
+                  }}
+                ></div>
+              </div>
+            </div>
           </div>
-          <div className="w-full bg-gray-200 rounded-full h-2.5">
-            <div
-              className="bg-blue-300 h-2.5 rounded-full"
-              style={{ width: "45%" }}
-            ></div>
-          </div>
-        </div>
+        ) : (
+          <Link
+            to={`/courses/${course?.key}`}
+            className="inline-flex items-center justify-center px-5 py-3 mt-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 bg-gray-300 focus:ring-4"
+          >
+            View details
+          </Link>
+        )}
       </div>
     </div>
   );
